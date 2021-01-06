4a6307499e0ac55ff6bc5de97802688776ba1af2
Ramachandra Reddy <ramachandra.reddy@kaiostech.com>
2021-01-06 18:26:16 +0530

    Returning output in json format

--- src/cheerio.js
+++ src/cheerio.js
@@ -2,11 +2,26 @@ import cheerio from "cheerio";
 
 export const loadDom = (html) => cheerio.load(html);
 
+let movieData = [];
+
 export const getValuesFromDomNodes = (dom) => {
   let $ = dom;
-  let base = $(".lister-list");
-  let title = base.find(".lister-item-header > a").html();
-  let year = base.find(".lister-item-year").html();
+  $(".lister-list .lister-item").each(function (i, elm) {
+    let title = $(elm).find(".lister-item-header > a").text();
+    let year = $(elm).find(".lister-item-year").text();
+    let certificate = $(elm)
+      .find(".lister-item-content .text-muted > .certificate")
+      .text();
+    let duration = $(elm).find(".text-muted > .runtime").text();
+    let obj = {
+      item: i,
+      title,
+      year,
+      certificate,
+      duration,
+    };
+    movieData.push(obj);
+  });
 
-  return console.log({ title, year });
+  return console.log(JSON.stringify(movieData));
 };
